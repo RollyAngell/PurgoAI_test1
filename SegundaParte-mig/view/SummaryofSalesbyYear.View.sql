4d9782f84ffd901853e2166c99df99bf59cd50cc
Purgo AI <support@purgo.ai>
2024-10-02 16:58:38 -0500

    Commit message

---
+++ SegundaParte-mig/view/SummaryofSalesbyYear.View.sql
@@ -0,0 +1,5 @@
+
+CREATE OR REPLACE VIEW `Summary_of_Sales_by_Year` AS
+SELECT Orders.ShippedDate, Orders.OrderID, `Order_Subtotals`.Subtotal
+FROM Orders INNER JOIN `Order_Subtotals` ON Orders.OrderID = `Order_Subtotals`.OrderID
+WHERE Orders.ShippedDate IS NOT NULL
