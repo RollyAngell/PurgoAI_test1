af89df63010ea85fdc5e0481c6086e582285144e
Purgo AI <support@purgo.ai>
2024-10-02 16:58:06 -0500

    Commit message

---
+++ SegundaParte-mig/table/Products.Table.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE Products (
+  ProductID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+  ProductName STRING NOT NULL,
+  SupplierID INTEGER,
+  CateryID INTEGER,
+  QuantityPerUnit STRING,
+  UnitPrice DECIMAL(19,4) DEFAULT 0,
+  UnitsInStock SMALLINT DEFAULT 0,
+  UnitsOnOrder SMALLINT DEFAULT 0,
+  ReorderLevel SMALLINT DEFAULT 0,
+  Discontinued BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (ProductID)
+);
+
+ALTER TABLE Products
+  ADD CONSTRAINT FK_Products_Cateries FOREIGN KEY (CateryID) REFERENCES Cateries (CateryID);
+
+ALTER TABLE Products
+  ADD CONSTRAINT FK_Products_Suppliers FOREIGN KEY (SupplierID) REFERENCES Suppliers (SupplierID);
